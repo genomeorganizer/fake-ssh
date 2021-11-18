9e1a74a733615c83b359cd6ba928a6b7b1651bd6
GenomeOrganizer <californiasteven@outlook.com>
2021-11-18 14:16:13 -0800

    updated

--- fake_ssh.py
+++ fake_ssh.py
@@ -9,9 +9,7 @@ import traceback
 import paramiko
 import json
 from getmac import get_mac_address
-from os.path import expanduser
-home = expanduser("~")
-default_path=os.path.join(home,".mail")
+
 import email_alerts
 
 def load_auth_file(filename):
@@ -22,50 +20,25 @@ def load_auth_file(filename):
 LOG = open("/usr/local/bin/fake-ssh/logs/log.txt", "a")
 HOST_KEY = paramiko.RSAKey(filename='/usr/local/bin/fake-ssh/keys/private.key')
 SSH_BANNER = "SSH-2.0-OpenSSH_8.2p1 Ubuntu-4ubuntu0.1"
-MOTD="""###############################################################
-#                                                   Welcome to Ubuntu Server 20.0.1                                                             # 
-#                                    All connections are monitored and recorded                                       #
-#                           Disconnect IMMEDIATELY if you are not an authorized user!                  #
-###############################################################
 
-"""
-PROMPT="~ $"
 
 def handle_cmd(cmd, chan):
-    try:
-        """Branching statements to handle and prepare a response for a command"""
-        response = ""
-        if cmd.startswith("sudo"):
-            send_ascii("sudo.txt", chan)
-            return
-        elif cmd.startswith("ls"):
-            response = "Desktop Documents Downloads Music Pictures Public Templates"
-        elif("version" in cmd):
-            response = """GNU bash, version 3.1.57(1)-release (x86_64)
-    Copyright (C) 2007 Free Software Foundation, Inc."""
-        elif cmd.startswith("pwd"):
-            response = "/home/clippy"
-        elif cmd.startswith("cd"):
-            send_ascii("cd.txt", chan)
-            return
-        elif cmd.startswith("cat"):
-            send_ascii("cat.txt", chan)
-            return
-        elif cmd.startswith("rm"):
-            response = "rm: cannot remove '{}': No such file or directory".format(cmd.split(rm)[1])
-            return
-        elif cmd.startswith("whoami"):
-            send_ascii("wizard.txt", chan)
-            response = "You are a wizard of the internet!"
-            return
-        else:
-        response = "bash: {} command not found".format(cmd)
-    except:
-        response = "bash: {} command not found".format(cmd)
+    """Branching statements to handle and prepare a response for a command"""
+    response = ""
+    if cmd.startswith("ls"):
+        response = "Desktop Documents Pictures Music Shared"
+    elif cmd.startswith("version"):
+        response = """GNU bash, version 3.1.27(1)-release (x86_64)
+Copyright (C) 2007 Free Software Foundation, Inc."""
+    elif cmd.startswith("pwd"):
+        response = "/home/user"
+    elif cmd.startswith("rm"):
+        response = "-bash: {} not found"
+    else:
+        response = "-bash: "+cmd+" command not found"
     LOG.write(response + "\n")
     LOG.flush()
     chan.send(response + "\r\n")
-
 
 
 def send_ascii(filename, chan):
@@ -110,14 +83,12 @@ def handle_connection(client, addr):
     msg = "Connection from ip: "+ip+" mac: "+mac
     LOG.write("\n\n"+msg+"\n")#Connection from: " + addr[0] + "\n")
     print('Got a connection!')
-    #a=load_auth_file("/usr/local/bin/email.json")
-    a=default_path
+    a=load_auth_file("/usr/local/bin/email.json")
     try:
-        email_alerts.send(auth=a,to="tosteveayers@outlook.com",subject="ALERT! SSH Connection attempt to fake-ssh on port 22 from: " + addr[0],message=msg)
-        # print("Sent email")
+        email_alerts.send(auth=a,to=mail,subject="ALERT! SSH Connection attempt to fake-ssh on port 22 from: " + addr[0],message=msg)
+        print("Sent email")
     except:
-        # print("unable to send alert")
-        exit(0)
+        print("unable to send alert")
     try:
         transport = paramiko.Transport(client)
         transport.add_server_key(HOST_KEY)
@@ -141,10 +112,15 @@ def handle_connection(client, addr):
             raise Exception("No shell request")
 
         try:
-            chan.send(MOTD)
+            chan.send("""###############################################################\r\n
+    Welcome to Ubuntu Server Version 20.0.1\r\n
+    All connections are monitored and recorded\r\n
+    Disconnect IMMEDIATELY if you are not an authorized user!\r\n
+###############################################################\r\n
+            \r\n""")
             run = True
             while run:
-                chan.send(PROMPT)
+                chan.send("$ ")
                 command = ""
                 while not command.endswith("\r"):
                     transport = chan.recv(1024)
@@ -212,5 +188,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run a fake ssh server')
     parser.add_argument("--port", "-p", help="The port to bind the ssh server to (default 22)", default=22, type=int, action="store")
     parser.add_argument("--bind", "-b", help="The address to bind the ssh server to", default="", type=str, action="store")
+    parser.add_argument("--mail", "-m", help="notification email", default="", type=str, action="store")
     args = parser.parse_args()
+    mail = args.mail
     start_server(args.port, args.bind)
